b6f9feca8ffad7660002202cc13435cebe3ec92e
me19 <me19@pomscloud.ie>
2020-11-30 18:16:50 +0100

    Comment conflict

---
+++ install.sh
@@ -0,0 +1,7 @@
+#https://makandracards.com/makandra/29465-install-chromedriver-on-linux
+#https://www.python.org/downloads/
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+sudo apt-get install python3-tk -y
+python3 -m pip install selenium
+python3 -m pip install pysimplegui
